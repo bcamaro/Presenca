a53c73b629b8e5ff6f7091cf12f3352cb6542afc
bcamaro <165781421+bcamaro@users.noreply.github.com>
2024-04-15 00:22:18 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,11 @@
+﻿window.blazorDownloadExcelFile = (data, fileName) => {
+    const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' }); // Define o tipo de conteúdo como Excel
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+};
